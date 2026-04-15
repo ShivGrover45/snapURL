eca22b613a1ef46bdd5296743c747d3fe393ea04
ShivGrover45 <codershiv16@gmail.com>
2026-04-15 20:55:45 +0530

    database added

---
+++ server.js
@@ -0,0 +1,9 @@
+const app=require('./src/app')
+const connectDB = require('./src/db/db')
+require('dotenv').config()
+
+const port=process.env.PORT
+connectDB()
+app.listen(port,()=>{
+    console.log(`Server running on port ${port}`)
+})
