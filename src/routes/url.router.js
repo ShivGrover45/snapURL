3505cce21aa906d82ec9f31069de58fe298ffcaa
ShivGrover45 <codershiv16@gmail.com>
2026-04-16 17:47:55 +0530

    issue fixed

--- src/routes/url.router.js
+++ src/routes/url.router.js
@@ -2,8 +2,8 @@ const express=require('express')
 const { generateShortUrl, getShortUrl, getAnalytics } = require('../controller/url.controller')
 const Router=express.Router()
 
-Router.post('/link',generateShortUrl)
-Router.get('/:shortID',getShortUrl)
-Router.get('/analytics',getAnalytics)
+Router.post('/link', generateShortUrl)
+Router.get('/analytics/:shortID', getAnalytics)  // ← move up + add :shortID
+Router.get('/:shortID', getShortUrl)
 
 module.exports=Router
