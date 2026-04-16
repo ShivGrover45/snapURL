49aacfcad0441df0f9b1a85d9fe6871761e7cf64
ShivGrover45 <codershiv16@gmail.com>
2026-04-16 16:43:55 +0530

    bug fixed

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 const express=require('express')
 const Router = require('./routes/url.router')
+const cors=require('cors')
 const app=express()
 
 app.use(express.json())
 app.use('/api',Router)
-app.use(express.cors())
+app.use(cors())
 module.exports=app
