7cedd2f510d6c82ff431425f140b39bea6c118b5
ShivGrover45 <codershiv16@gmail.com>
2026-04-16 17:23:15 +0530

    short url fixed

--- src/controller/url.controller.js
+++ src/controller/url.controller.js
@@ -18,7 +18,7 @@ const generateShortUrl=async(req,res)=>{
     console.log(newURl)
     res.status(200).json({
         message:"short url successfully created",
-        link:`localhost:3000/:${newURl.shortID}`
+        link:`process.env.BASE_URL/:${newURl.shortID}`
     })
     }catch(err){
         console.log(err)
