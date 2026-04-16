9fe61c27408988aa9d260fff0e43fe77e409f060
ShivGrover45 <codershiv16@gmail.com>
2026-04-16 20:31:18 +0530

    rate limit added

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 const express=require('express')
 const Router = require('./routes/url.router')
 const cors=require('cors')
+const rateLimit=require('express-rate-limit')
 const app=express()
+
+const limiter=rateLimit({
+    windowMs:5*60*100,
+    max:10,
+    message:"Too many requests try again later"
+})
 
 app.use(cors())
 app.use(express.json())
-app.use('/api',Router)
+app.use('/api',limiter,Router)
 
 module.exports=app
